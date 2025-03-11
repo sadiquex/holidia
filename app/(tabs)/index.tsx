b5ecf554fd8095c2912cebf9e60ebca4050f3ea4
sadiquex <abubakasaddik1@gmail.com>
2025-03-11 10:19:59 +0000

    add reusable text component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,19 @@
-import { Stack } from 'expo-router';
 import { StyleSheet, Text, View } from 'react-native';
 
 import { Container } from '~/components/container';
+import CustomText from '~/components/text';
 
 export default function Home() {
   return (
     <Container>
-      <Text className="text-primary text-2xl">Home</Text>
+      <CustomText variant="title">title</CustomText>
+      <CustomText variant="body">body</CustomText>
+      <CustomText variant="body-primary">body primary</CustomText>
+      <CustomText variant="button">button</CustomText>
+      <CustomText variant="caption">caption</CustomText>
+      <CustomText variant="caption-primary">caption primary</CustomText>
+      <CustomText variant="display">display</CustomText>
+      <CustomText variant="subtitle">subtitle</CustomText>
     </Container>
   );
 }
