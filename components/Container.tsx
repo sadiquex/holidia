a19f19c4aad5be45f67b7099f50d69462a7b6c5d
sadiquex <abubakasaddik1@gmail.com>
2025-04-04 12:36:58 +0000

    Remove incorrectly cased file

--- components/Container.tsx
+++
@@ -1,9 +0,0 @@
-import { SafeAreaView } from 'react-native';
-
-export const Container = ({ children }: { children: React.ReactNode }) => {
-  return <SafeAreaView className="flex flex-1">{children}</SafeAreaView>;
-};
-
-const styles = {
-  container: 'flex flex-1',
-};
